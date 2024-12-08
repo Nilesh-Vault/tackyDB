075f4beca3f89631f1ac79f873980d3f35ebcb0c
Asad <asad200ana@gmail.com>
2024-12-08 16:40:25 +0530

    Added deleteDB command

--- src/DatabaseCore.py
+++ src/DatabaseCore.py
@@ -1,8 +1,10 @@
 import csv
 import os
+import shutil
 
 
 class DatabaseCore:
+    @staticmethod
     def createDatabase(db_name: str) -> str:
         if not db_name.isalnum():
             return "Error: Database name must be alphanumeric and cannot contain special characters"
@@ -19,6 +21,7 @@ class DatabaseCore:
         except OSError as e:
             return f"Error: {str(e)}\n"
 
+    @staticmethod
     def createTable(db_name: str, table_name: str, columns: dict) -> str:
         root_path: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         db_path: str = os.path.join(root_path, f"database/{db_name.lower()}")
@@ -68,6 +71,7 @@ class DatabaseCore:
         except Exception as e:
             return f"Error:Failed to create table '{table_name}\n{str(e)}\n"
 
+    @staticmethod
     def deleteTable(db_name: str, table_name: str) -> str:
         root_path: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         db_path: str = os.path.join(root_path, f"database/{db_name.lower()}")
@@ -85,3 +89,21 @@ class DatabaseCore:
             return f"Table '{table_name}' deleted successfully at {db_path}"
         else:
             return f"Table '{table_name}' does not exist {db_path}'"
+    
+    @staticmethod
+    def deleteDB(db_name: str) -> str:
+        root_path: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        db_path: str = os.path.join(root_path, f"database/{db_name.lower()}")
+
+        if not os.path.exists(db_path):
+            return f"Error: Database '{db_name}' does not exist."
+
+        confirmation = input(f"Are you sure you want to delete the database '{db_name}'? Type '{db_name}' to confirm: ")
+        if confirmation != db_name:
+            return "Error: Database deletion aborted. The names do not match."
+
+        try:
+            shutil.rmtree(db_path)
+            return f"Database '{db_name}' and all its contents have been successfully deleted."
+        except Exception as e:
+            return f"Error: Failed to delete the database '{db_name}'. {str(e)}"
